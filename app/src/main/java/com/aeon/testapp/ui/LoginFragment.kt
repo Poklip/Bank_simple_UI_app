df9ea3944b846aa6f09f655a5632196395578c24
poklip <qpokusnik@gmail.com>
2023-11-28 09:47:14 -0800

    *WIP* login func has been added

--- app/src/main/java/com/aeon/testapp/ui/LoginFragment.kt
+++ app/src/main/java/com/aeon/testapp/ui/LoginFragment.kt
@@ -6,8 +6,13 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.aeon.testapp.App
 import com.aeon.testapp.R
 import com.aeon.testapp.data.models.LoginInfo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
@@ -16,8 +21,25 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
     private val etPassword: EditText by lazy { requireActivity().findViewById(R.id.etPassword) }
     private val btnLogin: Button by lazy { requireActivity().findViewById(R.id.btnLogin) }
     private val tvError: TextView by lazy { requireActivity().findViewById(R.id.tvError) }
+    private val networkScope = CoroutineScope(Dispatchers.IO)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val tempLogin = etLogin.text.toString()
+        val tempPassword = etPassword.text.toString()
+
+        btnLogin.setOnClickListener {
+            login(LoginInfo(tempLogin, tempPassword))
+        }
+    }
+
+    private fun login(loginInfo: LoginInfo) {
+        networkScope.launch {
+            withContext(Dispatchers.IO) {
+                val token = App().getApi().login(loginInfo).execute()
+                //TODO(look at the error body)
+            }
+        }
     }
 }
